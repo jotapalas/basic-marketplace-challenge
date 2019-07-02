3d499cd5e6a6f815338c8076c8bae7dad1889e33
Juan Palacios <palaciosgalvanj@gmail.com>
2019-07-02 18:52:44 +0200

    Create simple GET endpoints

--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ let baskets = [];
 app.post('/basket', (req, res, next) => {
   let basket = req.basket || {};
   baskets.push(basket);
-  res.send(baskets);
-})
+  res.send(200, basket);
+});
+
+app.get('/basket/:id', (req, res, next) => {
+  let basketId = req.params.id;
+  let code = basketId && basketId < baskets.length ? 200 : 404;
+  let response = code === 200
+               ? baskets[basketId]
+               : { 'error': 'Basket not found' }
+  res.send(code, response);
+});
+
+app.get('/basket', (req, res, next) => {
+  res.send(200, baskets);
+});
 
 app.listen(3000, () => {
  console.log('Server running on port 3000');
