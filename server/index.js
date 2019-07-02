f404a020b4e9e4f37ad51e26084af45a1c0b6e68
Juan Palacios <palaciosgalvanj@gmail.com>
2019-07-02 19:42:36 +0200

    Add DELETE endpoint for basket

--- server/index.js
+++ server/index.js
@@ -1,25 +1,47 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-let baskets = [];
+let baskets = new Map();
+
+app.use(bodyParser.json());
 
 app.post('/basket', (req, res, next) => {
-  let basket = req.basket || {};
-  baskets.push(basket);
-  res.send(200, basket);
+  let basket = req.body.basket || {};
+  let code = basket.id ? 405 : 200;
+  let response = { 'error' : 'ID not allowed in a POST request. Use PUT instead.' };
+  if (code == 200) {
+    basket.id = baskets.size + 1;
+    response = basket;
+    baskets.set(basket.id, basket);
+  }
+  res.status(code).send(response);
 });
 
 app.get('/basket/:id', (req, res, next) => {
-  let basketId = req.params.id;
-  let code = basketId && basketId < baskets.length ? 200 : 404;
+  let basketId = parseInt(req.params.id);
+  let code = basketId && baskets.has(basketId) ? 200 : 404;
   let response = code === 200
-               ? baskets[basketId]
+               ? baskets.get(basketId)
                : { 'error': 'Basket not found' }
-  res.send(code, response);
+  res.status(code).send(response);
 });
 
 app.get('/basket', (req, res, next) => {
-  res.send(200, baskets);
+  res.status(200).send(baskets);
+});
+
+app.delete('/basket/:id', (req, res, next) => {
+  let basketId = parseInt(req.params.id);
+  console.log(req.params.id);
+  console.log(baskets.has(basketId));
+  let code = baskets.has(basketId) ? 200 : 404;
+  let response = { 'error': 'Basket not found' }; 
+  if (code === 200) {
+    response = baskets.get(basketId);
+    baskets.delete(basketId);
+  }
+  res.status(code).send(response);
 });
 
 app.listen(3000, () => {
